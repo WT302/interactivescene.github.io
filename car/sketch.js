9a229a0ff5e425c42d4f65ec4585365792c20bc7
Wang7004898 <7004898@spsd.sk.ca>
2025-11-06 11:32:55 -0600

    u

--- car/sketch.js
+++ car/sketch.js
@@ -9,91 +9,145 @@ let westbound = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // testCar = new Vehicle(0, color(255,0,0),100, 150,1,2); 
+
   // eastbound
-  for(let i = 0; i < 15; i++){
-    eastbound.push(new Vehicle(0, color(random(255), random(255), random(255)), random(width), windowHeight / 2 + 100, 1, random(2,5)));
+  for (let i = 0; i < 15; i++) {
+    eastbound.push(
+      new Vehicle(
+        0,
+        color(random(255), random(255), random(255)),
+        random(width),
+        windowHeight / 2 + 100,
+        1,
+        random(2, 5)
+      )
+    );
   }
-  //westbound
-  for(let i = 0; i < 15; i++){
-    westbound.push(new Vehicle(1, color(random(255), random(255), random(255)), random(width), windowHeight / 2 - 100, 0, random(-5,-2)));
+
+  // westbound
+  for (let i = 0; i < 15; i++) {
+    westbound.push(
+      new Vehicle(
+        1,
+        color(random(255), random(255), random(255)),
+        random(width),
+        windowHeight / 2 - 100,
+        0,
+        random(-5, -2)
+      )
+    );
   }
+
 }
 
 function draw() {
   background(220);
-  fill(80,80,80);
+  fill(80, 80, 80);
   drawRoad();
-  // testCar.display();
-  for(let car of eastbound){
-    car.action();
+
+  // process all eastbound vehicles
+  for (let car of eastbound) {
+    car.display(); 
   }
-  for(let car of westbound){
-    car.action();
+
+  // process all westbound vehicles
+  for (let car of westbound) {
+    car.display();
   }
 }
 
-function drawRoad(){
+// draw road with lane divider
+function drawRoad() {
   noStroke();
-  rect(0, windowHeight/4, windowWidth, 500);
-  for(let x = 0; x < windowWidth; x += 100){
-    fill(255,255,255);
-    rect(x,windowHeight/2, 70, 10);
+  fill(80, 80, 80);
+  rect(0, windowHeight / 4, windowWidth, 500);
+
+  // dashed white line in middle
+  fill(255);
+  for (let x = 0; x < windowWidth; x += 100) {
+    rect(x, windowHeight / 2, 70, 10);
   }
 }
 
+// click to add new cars
+function mousePressed() {
+  // Shift + click  westbound
+  if (keyIsDown(SHIFT)) {
+    westbound.push(
+      new Vehicle(
+        1,
+        color(random(255), random(255), random(255)),
+        mouseX,
+        windowHeight / 2 - 100,
+        0,
+        random(-5, -2)
+      )
+    );
+  } else {
+    // Normal click eastbound
+    eastbound.push(
+      new Vehicle(
+        0,
+        color(random(255), random(255), random(255)),
+        mouseX,
+        windowHeight / 2 + 100,
+        1,
+        random(2, 5)
+      )
+    );
+  }
+}
+
+
+// Vehicle Class
 class Vehicle {
-  constructor(type, col, x, y, direction, xSpeed){
-    this.type = type;  // 0 = car, 1 = truck
-    this.col = col; //color
-    this.x = x; //x position
-    this.y = y; //y position
+  constructor(type, col, x, y, direction, xSpeed) {
+    this.type = type; // 0 = car, 1 = truck
+    this.col = col; // color
+    this.x = x; // x position
+    this.y = y; // y position
     this.direction = direction; // 0 = left, 1 = right
     this.xSpeed = xSpeed; // speed
   }
-  
-  display(){
+
+  display() {
     fill(this.col);
     noStroke();
-    if (this.type === 0){
+    if (this.type === 0) {
       rect(this.x, this.y, 40, 20); // car
-    }
-    else{
+    } else {
       rect(this.x, this.y, 60, 25); // truck
-    } 
-  } 
-
-  //move
-  move(){
-    this.x == this.xSpeed;
-    //other side
-    if(this.x > width) this.x = -60;
-    if(this.x < -60) this.x = width;
+    }
   }
 
-  //speed up
-  speedUp(){
-    if(this.xSpeed < 10 && this.direction === 1)this.xSpeed += 0.2;
-    if(this.xSpeed > -10 && this.direction === 0)this.xSpeed -= 0.2;
+  move() {
+    this.x += this.xSpeed; // fixed
+    // wrap around
+    if (this.x > width) this.x = -60;
+    if (this.x < -60) this.x = width;
   }
 
-  //Down
-  speedDown(){
+  speedUp() {
+    if (this.xSpeed < 10 && this.direction === 1) this.xSpeed += 0.2;
+    if (this.xSpeed > -10 && this.direction === 0) this.xSpeed -= 0.2;
+  }
+
+  speedDown() {
     if (this.direction === 1) this.xSpeed = max(this.xSpeed - 0.2, 1);
     else this.xSpeed = min(this.xSpeed + 0.2, -1);
   }
 
-  //switch color
-  changeColor(){
+  changeColor() {
     this.col = color(random(255), random(255), random(255));
   }
-  action(){
+
+  action() {
     this.move();
-    //1%
     if (random(100) < 1) this.speedUp();
     if (random(100) < 1) this.speedDown();
-    if (random(100 < 1)) this.changeColor();
+    if (random(100) < 1) this.changeColor();
     this.display();
   }
 }
+
 
