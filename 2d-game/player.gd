ebeda4f336a8b0880f695e238319ce7d4be093fa
WT302 <tonywang1608@gmail.com>
2026-01-08 12:03:39 -0600

    u

--- 2d-game/player.gd
+++ 2d-game/player.gd
@@ -2,21 +2,27 @@ extends CharacterBody2D
 
 const gravity = 2000
 const speed = 350
-const jump_force = 800
-var my_velocity = Vector2.ZERO
-var is_jumping = false
+const jump_velocity = 800
 
-func _physics_process(_delta):
-	my_velocity = move_and_slide()
-	if is_on_floor():
-		is_jumping =false
+@onready var animation_player = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
-func _input(event):
-	if event.is_action_pressed("jump") and not is_jumping:
-		velocity.y = - jump_force
-		is_jumping = true
-
-func _process(delta):
-	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	velocity.x = direction * speed
-	velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		print("jump!",velocity.y)
+	move_and_slide()
+	if not is_on_floor():
+		animation_player.play("jump")
+	elif direction == 0:
+		animation_player.play("idle")
+	else:
+		animation_player.play("walk")
+		sprite.flip_h = direction < 0
+			
